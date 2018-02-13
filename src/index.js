41ad9d1faa762e6ecbf340208fd3b7615f65e57f
acgotaku <acgotaku311@gmail.com>
2018-02-13 20:22:26 +0900

    add eslint standard

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-import './index.css';
+import './index.css'
 
 class Weather extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       weather: Object.create(null),
       current: 0,
@@ -28,23 +28,28 @@ class Weather extends React.Component {
           woeid: '44418'
         }
       ]
-    };
-    this.fetchWeather(this.state.citys[this.state.current].woeid);
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  fetchWeather(woeid) {
-    const q = `select item.condition from weather.forecast where woeid = ${woeid}`;
-    const URL = `https://query.yahooapis.com/v1/public/yql?format=json&q=${encodeURIComponent(q)}`;
+  componentWillMount () {
+    this.fetchWeather(this.state.citys[this.state.current].woeid)
+  }
+
+  fetchWeather (woeid) {
+    const q = `select item.condition from weather.forecast where woeid = ${woeid}`
+    const URL = `https://query.yahooapis.com/v1/public/yql?format=json&q=${encodeURIComponent(q)}`
     fetch(URL).then((response) => {
       if (response.ok) {
-        response.json().then((data)=>{
+        response.json().then((data) => {
           this.setState({
             weather: data.query.results.channel.item.condition,
             show: !this.state.show
           })
         })
       } else {
-        console.log(response);
+        console.log(response)
         this.setState({
           weather: {
             code: 3200,
@@ -54,7 +59,7 @@ class Weather extends React.Component {
         })
       }
     }).catch((err) => {
-      console.log(err);
+      console.log(err)
       this.setState({
         weather: {
           code: 3200,
@@ -65,29 +70,29 @@ class Weather extends React.Component {
     })
   }
 
-  toCelsius(temperature) {
-    return Number.parseInt((temperature - 32) / 1.8, 10);
+  toCelsius (temperature) {
+    return Number.parseInt((temperature - 32) / 1.8, 10)
   }
 
-  handleChange = (event, index, value) => {
+  handleChange (event, index, value) {
     this.setState({
       current: value,
       show: !this.state.show
     }, () => {
-      this.fetchWeather(this.state.citys[this.state.current].woeid);
-    });
+      this.fetchWeather(this.state.citys[this.state.current].woeid)
+    })
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  handleSubmit (event) {
+    event.preventDefault()
     this.setState({
       show: !this.state.show
     }, () => {
-      this.fetchWeather(this.state.citys[this.state.current].woeid);
-    });
+      this.fetchWeather(this.state.citys[this.state.current].woeid)
+    })
   }
 
-  render() {
+  render () {
     const citys = this.state.citys.map((info, index) => {
       return (
         <MenuItem value={index} key={index} primaryText={info.city} />
@@ -95,36 +100,36 @@ class Weather extends React.Component {
     })
 
     return (
-      <div className="weather">
-        <div className="weather-select">
+      <div className='weather'>
+        <div className='weather-select'>
           <MuiThemeProvider>
-          <form onSubmit={this.handleSubmit} className="weather-form">
-               <DropDownMenu value={this.state.current} onChange={this.handleChange}>
+            <form onSubmit={this.handleSubmit} className='weather-form'>
+              <DropDownMenu value={this.state.current} onChange={this.handleChange}>
                 {citys}
-               </DropDownMenu>
-               <RaisedButton type="submit" label="Submit" primary={true} />
-           </form>
-           </MuiThemeProvider>
+              </DropDownMenu>
+              <RaisedButton type='submit' label='Submit' primary />
+            </form>
+          </MuiThemeProvider>
         </div>
-        <div className="weather-content-wrapper">
+        <div className='weather-content-wrapper'>
           <div className={`weather-content ${this.state.show ? 'show' : 'loading'}`}>
-            <div className="weather-city">
+            <div className='weather-city'>
               {this.state.citys[this.state.current].city}
             </div>
-            <div className="weather-desc">
-              <div className="weather-icon">
-                <img src={this.state.weather.code ? `/images/${this.state.weather.code}.svg` : ''} alt="weather icon" />
+            <div className='weather-desc'>
+              <div className='weather-icon'>
+                <img src={this.state.weather.code ? `/images/${this.state.weather.code}.svg` : ''} alt='weather icon' />
               </div>
-              <div className="weather-info">
+              <div className='weather-info'>
                 <p> {this.state.weather.text} </p>
                 <p> {this.toCelsius(this.state.weather.temp)} °C </p>
               </div>
             </div>
           </div>
-          <img className={`weather-content-loading ${this.state.show ? 'hide' : ''}`} src="images/loading.svg" alt="loading icon" />
+          <img className={`weather-content-loading ${this.state.show ? 'hide' : ''}`} src='images/loading.svg' alt='loading icon' />
         </div>
       </div>
     )
   }
 }
-ReactDOM.render(<Weather />, document.getElementById("root"));
+ReactDOM.render(<Weather />, document.getElementById('root'))
